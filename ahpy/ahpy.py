2b52244e6fd0af9933c9ed262ac3c17f1ed24021
Philip.Griffith <philip.griffith@gmail.com>
2021-01-19 20:33:44 -0500

    Doc strings for Compare object complete

--- ahpy/ahpy.py
+++ ahpy/ahpy.py
@@ -1,34 +1,38 @@
 import bisect
 import itertools
-import warnings
 import json
+import warnings
 
 import numpy as np
-import scipy.optimize as sp
+import scipy.optimize as spo
 
 
 class Compare:
-    # TODO redo this
     """
-    This class computes the priority vector and consistency ratio of a positive
-    reciprocal matrix. The 'weights' property contains the priority vector as a dictionary
-    whose keys are criteria and whose values are the criteria's weights.
-    The 'consistency_ratio' property contains the computed consistency ratio of the input matrix as a float.
-    :param name: string, the name of the Compare object; if the object has a parent,
-        this name MUST be included as a criterion of its parent
-    :param comparisons:
-    :param order: list of strings, the criteria of the matrix, listed in the order desired
+    This class computes the priority vector and consistency ratio of a positive reciprocal matrix, created using a
+    dictionary of pairwise comparison values. Optimal values are computed for any missing pairwise comparisons.
+    The 'name' property is used to link the Compare object to its parent Compose object.
+    The 'weights' property contains the priority vector as a dictionary whose keys are the criteria and
+        whose values are the criteria's weights.
+    The 'consistency_ratio' property contains the computed consistency ratio of the priority vector as a float.
+    :param name: string, the name of the Compare object; if the object has a parent, this name MUST be included
+        as a criterion of its parent
+    :param comparisons: dictionary, a dictionary in one of two forms: (i) each key is a tuple of two criteria and
+        each value is their pairwise comparison value, or (ii) each key is a criteria as a string and each value
+        is that criteria's measured value
     :param precision: integer, number of decimal places of precision to compute both the priority
         vector and the consistency ratio; default is 4
-    :param iterations: integer, number of iterations before the compute_eigenvector function stops;
-        default is 100
-    :param random_index: string, the random index estimates used to compute the consistency ratio;
-        valid input: 'dd', 'saaty'; default is 'dd'; see the compute_consistency_ratio function for more
-        information regarding the different estimates
+    :param random_index: string, the random index estimates used to compute the consistency ratio
+        (see the compute_consistency_ratio function for more information regarding the different estimates);
+        valid input: 'dd', 'saaty'; default is 'dd'
+    :param iterations: integer, number of iterations before the compute_eigenvector function stops; default is 100
+    :param tolerance: float, the stopping criteria for the cycling coordinates algorithm; the algorithm stops when the
+        difference between the norms of two cycles of coordinates is less than this value; default is 0.0001
+    :param cr: boolean, override to enable computing priority vectors without a consistency ratio
     """
 
-    def __init__(self, name=None, comparisons=None, precision=4, random_index='dd',
-                 iterations=100, tolerance=0.0001, cr=True):
+    def __init__(self, name=None, comparisons=None,
+                 precision=4, random_index='dd', iterations=100, tolerance=0.0001, cr=True):
         self.name = name
         self.comparisons = comparisons
         self.precision = precision
@@ -61,8 +65,8 @@ class Compare:
 
     def check_size(self):
         """
-        Raises a Value Error if a consistency ratio is requested and the chosen random index
-        will not support the size of the resulting matrix.
+        Raises a ValueError if a consistency ratio is requested and
+        the chosen random index does not support the size of the matrix.
         """
         if not self.normalize and self.cr and ((self.random_index == 'saaty' and self.size > 15) or self.size > 100):
             msg = "The input matrix is too large and a consistency ratio cannot be computed.\n" \
@@ -106,8 +110,7 @@ class Compare:
 
     def build_matrix(self):
         """
-        Creates a correctly-sized numpy matrix of ones,
-        then fills the matrix with values from the 'pairs' dictionary.
+        Creates a correctly-sized numpy matrix of 1s, then fills the matrix with values from the 'pairs' dictionary.
         """
         self.matrix = np.ones((self.size, self.size))
         for pair, value in self.pairs.items():
@@ -116,14 +119,14 @@ class Compare:
 
     def build_normalized_matrix(self):
         """
-        Creates a correctly-sized numpy matrix of values from the input 'comparisons' dictionary.
+        Creates a numpy matrix of values from the input 'comparisons' dictionary.
         """
         self.matrix = np.array(tuple(value for value in self.comparisons.values()), float)
 
     def get_missing_comparisons(self):
         """
         Creates the 'missing comparisons' dictionary by populating its keys with the unique comparisons
-        that are missing from the input 'comparisons' dictionary and populating its values with 1s.
+        missing from the input 'comparisons' dictionary and populating its values with 1s.
         """
         missing_comparisons = [key for key, value in self.pairs.items() if not value]
         for criteria in missing_comparisons:
@@ -147,17 +150,19 @@ class Compare:
     def minimize_coordinate_values(self):
         """
         Computes the minimum value for each missing value from the 'missing_comparisons' dictionary
-        using the cyclic coordinates method as described in the paper by Bozóki et al.
+        using the cyclic coordinates method described in Bozóki et al.
         """
 
         def lambda_max(x, x_location):
             """
-            The function to be minimized. Finds the largest eigenvalue of a matrix given a missing value.
+            The function to be minimized. Finds the largest eigenvalue of a matrix.
+            :param x: float, the variable to be minimized
+            :param x_location: tuple, the matrix location of the variable to be minimized
             """
             inverse_x_location = x_location[::-1]
             self.matrix.itemset(x_location, x)
             self.matrix.itemset(inverse_x_location, np.reciprocal(float(x)))
-            return np.linalg.eigvals(self.matrix).max()
+            return np.max(np.linalg.eigvals(self.matrix))
 
         # The upper bound of the solution space is 10 times the largest value of the matrix.
         upper_bound = np.nanmax(self.matrix) * 10
@@ -167,14 +172,15 @@ class Compare:
             with warnings.catch_warnings():
                 warnings.filterwarnings('ignore', category=np.ComplexWarning)
                 self.set_matrix(comparison)
-                optimal_solution = sp.minimize_scalar(lambda_max, args=(comparison_location,),
-                                                      method='bounded', bounds=(0, upper_bound)).x.real
+                optimal_solution = spo.minimize_scalar(lambda_max, args=(comparison_location,),
+                                                       method='bounded', bounds=(0, upper_bound)).x.real
             self.missing_comparisons[comparison] = optimal_solution
 
     def set_matrix(self, comparison):
         """
         Sets the value of every missing comparison in the comparison matrix (other than the current comparison)
         to its current value in the 'missing_comparisons' dictionary or its reciprocal.
+        :param comparison: tuple, a key from the 'missing_comparisons' dictionary
         """
         for key, value in self.missing_comparisons.items():
             if key != comparison:
@@ -200,7 +206,7 @@ class Compare:
 
     def compute_priority_vector(self, matrix, iterations, comp_eigenvector=None):
         """
-        Sets the 'priority_vector' property of the Compare object.
+        Returns the priority vector of the Compare object.
         :param matrix: numpy matrix, the matrix from which to derive the priority vector
         :param iterations: integer, number of iterations to run before the function stops
         :param comp_eigenvector: numpy array, a comparison eigenvector used during recursion
@@ -209,35 +215,35 @@ class Compare:
         sq_matrix = np.linalg.matrix_power(matrix, 2)
         row_sum = np.sum(sq_matrix, axis=1)
         total_sum = np.sum(sq_matrix)
-        princ_eigenvector = np.divide(row_sum, total_sum)
+        principal_eigenvector = np.divide(row_sum, total_sum)
 
         # Create a zero matrix as the comparison eigenvector if this is the first iteration
         if comp_eigenvector is None:
             comp_eigenvector = np.zeros(self.size)
 
         # Compute the difference between the principal and comparison eigenvectors
-        remainder = np.subtract(princ_eigenvector, comp_eigenvector).round(self.precision)
+        remainder = np.subtract(principal_eigenvector, comp_eigenvector).round(self.precision)
 
         # If the difference between the two eigenvectors is zero (after rounding to the specified precision),
         # set the current principal eigenvector as the priority vector for the matrix...
         if not np.any(remainder):
-            return princ_eigenvector.round(self.precision)
+            return principal_eigenvector.round(self.precision)
 
         # ...else recursively run the function until either there is no difference between
         # the principal and comparison eigenvectors, or until the predefined number of iterations has been met,
         # in which case set the last principal eigenvector as the priority vector
         iterations -= 1
         if iterations > 0:
-            return self.compute_priority_vector(sq_matrix, iterations, princ_eigenvector)
+            return self.compute_priority_vector(sq_matrix, iterations, principal_eigenvector)
         else:
-            return princ_eigenvector.round(self.precision)
+            return principal_eigenvector.round(self.precision)
 
     def compute_consistency_ratio(self):
         """
         Sets the 'consistency_ratio' property of the Compare object, using random index estimates from
         Donegan and Dodd's 'A note on Saaty's Random Indexes' in Mathematical and Computer Modelling,
         15:10, 1991, 135-137 (doi: 10.1016/0895-7177(91)90098-R) by default (random_index='dd').
-        If the random index of the object equals 'saaty', use the estimates from
+        If the random index of the object equals 'saaty', uses the estimates from
         Saaty's Theory And Applications Of The Analytic Network Process, Pittsburgh: RWS Publications, 2005, pg. 31.
         """
         # A valid, square, reciprocal matrix with only one or two rows must be consistent
@@ -265,28 +271,37 @@ class Compare:
             estimate = (ri_dict[larger] - ri_dict[smaller]) / (larger - smaller)
             random_index = estimate * (self.size - smaller) + ri_dict[smaller]
 
-        lambda_max = np.linalg.eigvals(self.matrix).max()  # Find the Perron-Frobenius eigenvalue of the matrix
+        lambda_max = np.max(np.linalg.eigvals(self.matrix))  # Find the Perron-Frobenius eigenvalue of the matrix
         consistency_index = (lambda_max - self.size) / (self.size - 1)
         self.consistency_ratio = np.real(consistency_index / random_index).round(self.precision)
 
-    def report(self):
+    def report(self, silent=False):
+        """
+        Returns the key information of the Compare object as a JSON object, optionally printing it to the console.
+        :param silent: boolean, if True, does not print the report to the console; default is False
+        """
 
-        def convert_tuple_keys_to_str(tuple_keys):
+        def convert_to_json_format(input_dict):
+            """
+            Returns a dictionary as a list of JSON compatible objects.
+            :param input_dict: dictionary, the dictionary to be converted
+            """
             if self.normalize:
-                return list({k: v} for k, v in tuple_keys.items())
+                return list({k: v} for k, v in input_dict.items())
             else:
-                return list({','.join(k): v} for k, v in tuple_keys.items())
+                return list({','.join(k): v} for k, v in input_dict.items())
 
         report = json.dumps({'Name': self.name,
                              'Weights': self.weights[self.name],
                              'Consistency Ratio': self.consistency_ratio,
                              'Random Index': 'Donegan & Dodd' if self.random_index == 'dd' else 'Saaty',
                              'Comparisons': {
-                                 'Input': convert_tuple_keys_to_str(self.comparisons),
-                                 'Computed': convert_tuple_keys_to_str(self.missing_comparisons)
-                                             if self.missing_comparisons else None}
+                                 'Input': convert_to_json_format(self.comparisons),
+                                 'Computed': convert_to_json_format(self.missing_comparisons)
+                                 if self.missing_comparisons else None}
                              }, indent=4)
-        print(report)
+        if not silent:
+            print(report)
         return report
 
 
